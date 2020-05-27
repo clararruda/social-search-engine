50179cfacc1f8a14f7734c77698fa528cef81065
Maria Clara <mclararruda@gmail.com>
2020-05-26 21:00:37 -0300

    remoçao de comentarios desnecessarios

--- social-search-engine-front/src/App.js
+++ social-search-engine-front/src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import { List, Image } from 'semantic-ui-react'
 
 let mock = [{"nome":"Shop da Maquiagem","arroba":"@shop_da_maquiagem","link":"https://www.instagram.com/shop_da_maquiagem/?hl=pt-br","seguidores":"","seguindo":"","posts":"","avatar":"https://instagram.ffor2-1.fna.fbcdn.net/v/t51.2885-19/s150x150/53889238_292523341428671_8135769945368690688_n.jpg?_nc_ht=instagram.ffor2-1.fna.fbcdn.net&_nc_ohc=Po8qn2Ir2gwAX-w7zyI&oh=68e78ac58ccdcfa1c1e16e467ae2953f&oe=5ED95146"}
-,{"nome":"Maquiagem BR","arroba":"@maquiagem.br_","link":"https://www.instagram.com/maquiagem.br_/?hl=pt-br","seguidores":"478 mil seguidores","seguindo":"125 seguindo","posts":"1066 publicações ","avatar":"https://instagram.ffor2-1.fna.fbcdn.net/v/t51.2885-19/s150x150/92760093_2881808261896216_6131163735999381504_n.jpg?_nc_ht=instagram.ffor2-1.fna.fbcdn.net&_nc_ohc=-Q522duuHwsAX8Q8MDl&oh=14c787f900594813548ff123cd4acd8e&oe=5ED92926"}
 ,{"nome":"Maquiagem para Iniciantes","arroba":"@maquiagem.iniciantes","link":"https://www.instagram.com/maquiagem.iniciantes/?hl=pt-br","seguidores":"","seguindo":"","posts":"","avatar":"https://instagram.ffor2-1.fna.fbcdn.net/v/t51.2885-19/s150x150/46404607_275746819790942_2517828215393746944_n.jpg?_nc_ht=instagram.ffor2-1.fna.fbcdn.net&_nc_ohc=mYPGtNjSYQIAX_YHC9T&oh=86b81cab60c4f6432063e66ef09187b0&oe=5ED9621C"}
 ,{"nome":"Loucas Por Maquiagem","arroba":"@loucaspormaquiagem","link":"https://www.instagram.com/loucaspormaquiagem/?hl=pt","seguidores":"410.3 mil seguidores","seguindo":"2250 seguindo","posts":"9853 publicações ","avatar":"https://instagram.ffor2-1.fna.fbcdn.net/v/t51.2885-19/s150x150/29416362_2054486881488916_3232420935617216512_n.jpg?_nc_ht=instagram.ffor2-1.fna.fbcdn.net&_nc_ohc=8-pw0EreLYkAX9P7sJI&oh=e168368e98de8b60b075427a00d06cdd&oe=5ED927C4"}
 ,{"nome":"Tudo de Maquiagem Oficial","arroba":"@tudodemaquiagem","link":"https://www.instagram.com/tudodemaquiagem/?hl=pt-br","seguidores":"84.9 mil seguidores","seguindo":"57 seguindo","posts":"997 publicações ","avatar":"https://instagram.ffor2-1.fna.fbcdn.net/v/t51.2885-19/s150x150/79720653_825047417923840_5235923866672431104_n.jpg?_nc_ht=instagram.ffor2-1.fna.fbcdn.net&_nc_ohc=jV9YqTUiKbUAX8sbK0d&oh=4d8254020172148708c4206ca62d7471&oe=5EDA0FC9"}
@@ -12,21 +11,6 @@ let mock = [{"nome":"Shop da Maquiagem","arroba":"@shop_da_maquiagem","link":"ht
 ,{"nome":"Galeria de Maquiagem","arroba":"@galeriademaquiagem","link":"https://www.instagram.com/galeriademaquiagem/?hl=pt-br","seguidores":"15.4 mil seguidores","seguindo":"5035 seguindo","posts":"1652 publicações ","avatar":"https://instagram.ffor2-1.fna.fbcdn.net/v/t51.2885-19/s150x150/89451059_1397912893725269_7498352697918095360_n.jpg?_nc_ht=instagram.ffor2-1.fna.fbcdn.net&_nc_ohc=ALzj3HQPMBUAX_Po5GL&oh=c76e03d10dfb561d87fff40d4a5ea087&oe=5ED75C5E"}
 ,{"nome":"Bem Mulher Maquiagem","arroba":"@bemmulhermaquiagem","link":"https://www.instagram.com/bemmulhermaquiagem/?hl=pt-br","seguidores":"","seguindo":"","posts":"","avatar":"https://instagram.ffor2-1.fna.fbcdn.net/v/t51.2885-19/s150x150/88386336_195595448358011_545818192165994496_n.jpg?_nc_ht=instagram.ffor2-1.fna.fbcdn.net&_nc_ohc=QnNv-YdWRt0AX8uJdsj&oh=d8e85f4350591446d4a3562dffa98aed&oe=5ED6EF35"}]
 
-
-// function ListExampleImage(props){
-//   let mock = props.mock
-//   const listItens = mock.map((nome) => nome)
-//   return (
-//     <List.Item>
-//       <Image avatar src={props.avatar}/>
-//       <List.Content>
-//         <listItens>
-//           <List.Header as='a'>{props.nome}</List.Header>
-//         </listItens>
-//       </List.Content>
-//     </List.Item>
-//   );
-// }
 
 class App extends Component{
 
@@ -39,10 +23,10 @@ class App extends Component{
           <ul>
             {mock.map((mock, indice) => {
               return(
-              <List.Item key={mock.arroba}>
-                <Image avatar src={mock.avatar}/>
-                {mock.arroba}
-              </List.Item>  
+                <List.Item key={mock.arroba}>
+                  <Image avatar src={mock.avatar}/>
+                  {mock.arroba}
+                </List.Item>  
               )
             }
             )}
